ba3e2f67e7d857282b61e6890cc28e5ad757b890
Milla Laaja <mmalinen78@gmail.com>
2022-10-08 11:56:34 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+  const [ userText, setUserText ] = useState('')
+
+  const textToVoice = () => {
+    console.log('painettu')
+    const thingToSay = userText;
+    Speech.speak(thingToSay);
+    setUserText('');
+    console.log(Speech.maxSpeechInputLength);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setUserText(text)}
+        value={userText}
+        placeholder='write here'
+      ></TextInput>
+      <Button
+        title='press to hear text'
+        onPress={textToVoice}
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +40,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    heigh: 20,
+    width: 155,
+    borderWidth: 1,
+    borderColor: 'yellow',
+    padding: 2,
+    backgroundColor: 'yellow',
+    color: 'blue',
+    margin: 12
+  }
 });
